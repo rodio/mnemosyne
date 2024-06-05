62f8b5a148714fe4df020f322936575d2a47b4e3
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-06-05 15:57:08 +0200

    Initial Commit

---
+++ mnemosyne/mnemosyneApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnemosyneApp.swift
+//  mnemosyne
+//
+//  Created by Rodion Borovyk on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct mnemosyneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

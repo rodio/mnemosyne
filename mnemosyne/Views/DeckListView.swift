04afba0e237ab1f5d31e5568cfc9e627f385b69b
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-06-25 21:05:58 +0200

    AddDeckSheet

--- mnemosyne/Views/DeckListView.swift
+++ mnemosyne/Views/DeckListView.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct DeckListView: View {
     @Environment(\.colorScheme) var colorScheme
     @Query var deckModels: [DeckModel]
+    @State var isAddDeckSheetPresented = false
 
     var body: some View {
         NavigationSplitView {
@@ -21,11 +22,52 @@ struct DeckListView: View {
             .padding(.top, 30)
             .listRowSpacing(30)
             .scrollContentBackground(.hidden)
+            .sheet(isPresented: $isAddDeckSheetPresented) {
+                AddDeckSheet()
+            }
+
+            Spacer()
+
+            Button("Add Deck") {
+                isAddDeckSheetPresented = true
+            }
         }
         detail: {
             Text("Select a deck")
         }
         .accentColor(colorScheme == .light ? .black : .white)
+    }
+}
+
+struct AddDeckSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) var context
+
+    @State private var deckName: String = ""
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Deck Name", text: $deckName)
+            }
+            .navigationTitle("New Deck")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let deck = DeckModel(name: deckName, cards: [CardModel(frontText: "Front", backText: "Back")])
+                        context.insert(deck)
+                        dismiss()
+                    }
+                }
+
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 

046a11edf22eaf06fcaa326fede452c6b53353b1
Rodion Borovyk <rodion.borovyk@gmail.com>
2024-07-12 14:06:30 +0200

    simple tests for the manager class

--- mnemosyneTests/mnemosyneTests.swift
+++ mnemosyneTests/mnemosyneTests.swift
@@ -9,28 +9,61 @@ import XCTest
 @testable import mnemosyne
 
 final class mnemosyneTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func createCardModels(n: Int) -> [CardModel] {
+        var cardModels : [CardModel] = []
+        for i in 1...n {
+            cardModels.append(CardModel(frontText: String(i), backText: String(i)))
         }
+        return cardModels
+    }
+    
+    func testWorksWithNoCards() throws {
+        let cardModels : [CardModel] = []
+        let manager = CardViewModelManager(cardModels: cardModels)
+        assert(manager.cardViewModels.count == 0)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 0)
+    }
+    
+    func testWorksWithOneCard() throws {
+        let cardModels = createCardModels(n: 1)
+        let manager = CardViewModelManager(cardModels: cardModels)
+        assert(manager.cardViewModels.count == 1)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 0)
+    }
+    
+    func testWorksWithNoMoreCards() throws {
+        let cardModels = createCardModels(n: 5)
+        let manager = CardViewModelManager(cardModels: cardModels)
+        assert(manager.cardViewModels.count == 5)
+        manager.removeCardViewModel()
+        manager.removeCardViewModel()
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 2)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 1)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 0)
     }
 
+    func testCreatesOnlyFive() throws {
+        let cardModels = createCardModels(n: 6)
+        let manager = CardViewModelManager(cardModels: cardModels)
+        assert(manager.cardViewModels.count == 5)
+    }
+    
+    func testAddsWhenTwoLeft() throws {
+        let cardModels = createCardModels(n: 10)
+        let manager = CardViewModelManager(cardModels: cardModels)
+        assert(manager.cardViewModels.count == 5)
+        
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 4)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 3)
+        manager.removeCardViewModel()
+        assert(manager.cardViewModels.count == 5)
+    }
 }
